5ab6e0ce580b88f6fc76507bc1080c4ad7086c5d
Michael W Powell <mwpowellhtx@gmail.com>
2019-10-25 12:59:42 -0400

    added a handful of build locator tests increasing our confidence with the framework

---
+++ src/Kingdom.Roslyn.Compilation.Services.Tests/MicrosoftBuildTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace Kingdom.Roslyn.Compilation.Services
+{
+    using Microsoft.Build.Locator;
+    using Xunit;
+    using Xunit.Abstractions;
+    using static Microsoft.Build.Locator.DiscoveryType;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <see cref="!:https://github.com/Microsoft/MSBuildLocator"/>
+    /// <see cref="!:https://docs.microsoft.com/en-us/visualstudio/msbuild/updating-an-existing-application#use-microsoftbuildlocator"/>
+    public class MicrosoftBuildTests : TestFixtureBase
+    {
+        public MicrosoftBuildTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        private const DiscoveryType DiscoveryTypes = DeveloperConsole | DotNetSdk | VisualStudioSetup;
+
+        private static VisualStudioInstanceQueryOptions Options => new VisualStudioInstanceQueryOptions {DiscoveryTypes = DiscoveryTypes};
+
+        [Fact]
+        public void Verify()
+        {
+            var instances = MSBuildLocator.QueryVisualStudioInstances(Options).ToArray();
+
+            foreach (var x in instances)
+            {
+                var report = GetRange(
+                    $"{nameof(x.DiscoveryType)}: {x.DiscoveryType}"
+                    , $"{nameof(x.MSBuildPath)}: {x.MSBuildPath}"
+                    , $"{nameof(x.Name)}: {x.Name}"
+                    , $"{nameof(x.Version)}: {x.Version}"
+                    , $"{nameof(x.VisualStudioRootPath)}: {x.VisualStudioRootPath}"
+                );
+
+                report.ToList().ForEach(OutputHelper.WriteLine);
+            }
+
+            MSBuildLocator.RegisterInstance(instances.ElementAt(0));
+        }
+    }
+}
