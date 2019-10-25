85799aecd69416850b2ffd5b9533c8b43869aaa9
Michael W Powell <mwpowellhtx@gmail.com>
2019-10-25 13:02:03 -0400

    refactored some bits to intermediate and base classes added work space diagnostic verification also improved compilation diagnostic verification

--- src/Kingdom.Roslyn.Compilation.Services.Tests/CodeGeneration/EmbeddedCodeGenerationTests.cs
+++ src/Kingdom.Roslyn.Compilation.Services.Tests/CodeGeneration/EmbeddedCodeGenerationTests.cs
@@ -3,14 +3,13 @@ using System.Linq;
 
 namespace Kingdom.Roslyn.Compilation.Services.CodeGeneration
 {
-    using Microsoft.CodeAnalysis.MSBuild;
+    using Microsoft.CodeAnalysis;
     using Xunit;
     using Xunit.Abstractions;
+    using static Microsoft.CodeAnalysis.WorkspaceDiagnosticKind;
 
-    public class EmbeddedCodeGenerationTests : CompilationCodeGenerationTestFixtureBase<MSBuildWorkspace, MSBuildCompilationManager>
+    public class EmbeddedCodeGenerationTests : MSBuildCompilationManagerTestFixtureBase
     {
-        protected override MSBuildCompilationManager CreateCompilationManager() => new MSBuildCompilationManager(Debug);
-
         public EmbeddedCodeGenerationTests(ITestOutputHelper outputHelper)
             : base(outputHelper)
         {
@@ -34,7 +33,12 @@ namespace Kingdom.Roslyn.Compilation.Services.CodeGeneration
             );
 
             context.RefreshContents();
-            CompilationManager.Workspace.OpenProjectAsync(context.ProjectPath).Wait();
+
+            var projectPath = context.ProjectPath.AssertNotNull().AssertFileExists();
+
+            CompilationManager.Workspace.OpenProjectAsync(projectPath).Wait();
+
+            EvaluateRoslynWorkspaceDiagnostics(context, CompilationManager.Workspace.Diagnostics.ToArray());
 
             CompilationManager.EvaluateCompilation += Private_OnEvaluateCompilation;
 
@@ -47,8 +51,12 @@ namespace Kingdom.Roslyn.Compilation.Services.CodeGeneration
         public void Test_Case_A()
         {
             var context = AppendContext(new ProjectContext(UseCases.A));
+
             context.RefreshContents();
+
             CompilationManager.Workspace.OpenProjectAsync(context.ProjectPath).Wait();
+
+            EvaluateRoslynWorkspaceDiagnostics(context, CompilationManager.Workspace.Diagnostics.ToArray());
 
             CompilationManager.EvaluateCompilation += Private_OnEvaluateCompilation;
 
@@ -57,24 +65,55 @@ namespace Kingdom.Roslyn.Compilation.Services.CodeGeneration
             CompilationManager.EvaluateCompilation -= Private_OnEvaluateCompilation;
         }
 
-
-        // TODO: TBD: we could potentially plant this in a base class...
-        private void Private_OnEvaluateCompilation(object sender, CompilationDiagnosticEventArgs e)
+        // TODO: TBD: could expose these as core API, etc...
+        private void EvaluateRoslynWorkspaceDiagnostics(ProjectContext _, IEnumerable<WorkspaceDiagnostic> diagnostics)
         {
+            /* TODO: TBD: there is no clear path here yet.
+             * https://gitter.im/dotnet/roslyn
+             * https://github.com/microsoft/msbuild/issues/4319 / ToolsVersion should be ignored by MSBuild task
+             * https://github.com/microsoft/msbuild/issues/4848 / SDK 'Microsoft.NET.Sdk' not found loading CSharp project via MSBuildWorkspace.OpenProjectAsync
+             * https://github.com/dotnet/roslyn/issues/39471 / SDK 'Microsoft.NET.Sdk' not found loading CSharp project via MSBuildWorkspace.OpenProjectAsync
+             * https://github.com/mwpowellhtx/Kingdom.Roslyn.Compilation.Services
+             * https://github.com/mwpowellhtx/Code.Generation.Roslyn
+             * https://github.com/mwpowellhtx/Kingdom.Roslyn.Compilation.Services/blob/master/src/Kingdom.Roslyn.Compilation.Services.Tests/CodeGeneration/EmbeddedCodeGenerationTests.cs#L10
+             */
             var i = 0;
 
-            var context = ProjectContexts.LastOrDefault().AssertNotNull();
+            // ReSharper disable PossibleMultipleEnumeration
+            foreach (var x in diagnostics.AssertNotNull())
+            {
+                OutputHelper.WriteLine($"{++i}: {x}");
+            }
 
-            var accepted = e.Filter.TryAcceptResult(out var result, out var diagnostics);
+            diagnostics.AssertFalse(x => x.Any(y => y.Kind == Failure));
+            // ReSharper restore PossibleMultipleEnumeration
+        }
 
-            /* We could get fancier and condense it further, but there is some benefit
-             * from a troubleshooting perspective in leaving it open such as it is. */
+        private void EvaluateRoslynDiagnostics(ProjectContext context, IEnumerable<Diagnostic> diagnostics)
+        {
+            var i = 0;
 
             foreach (var x in diagnostics)
             {
                 context.DiagnosticResults.Add(DiagnosticResult.Create(x));
                 OutputHelper.WriteLine($"{++i}: {context.DiagnosticResults.Last().Summary}");
             }
+        }
+
+        // TODO: TBD: we could potentially plant this in a base class...
+        private void Private_OnEvaluateCompilation(object sender, CompilationDiagnosticEventArgs e)
+        {
+            const DiagnosticSeverity warning = DiagnosticSeverity.Warning;
+
+            WorkspaceDiagnostic d;
+            var context = ProjectContexts.LastOrDefault().AssertNotNull();
+
+            var accepted = e.Filter.TryAcceptResult(out var result, warning, out var diagnostics);
+
+            /* We could get fancier and condense it further, but there is some benefit
+             * from a troubleshooting perspective in leaving it open such as it is. */
+
+            EvaluateRoslynDiagnostics(context, diagnostics);
 
             result.AssertTrue(x => x.Success);
             accepted.AssertTrue();
