77fa5b91b334e3463bb9729cb1878c99e5ef5a71
Michael W Powell <mwpowellhtx@gmail.com>
2019-11-14 10:55:12 -0500

    added range extension methods

---
+++ src/Kingdom.Roslyn.Compilation.Services.Tests/Extensions/EnumerableExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Kingdom.Roslyn.Compilation
+{
+    public static class EnumerableExtensionMethods
+    {
+        /// <summary>
+        /// Generates a Range of Values based on <paramref name="anchor"/> followed by zero
+        /// or more optional <paramref name="values"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="anchor"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ToRange<T>(this T anchor, params T[] values)
+        {
+            yield return anchor;
+
+            foreach (var x in values)
+            {
+                yield return x;
+            }
+        }
+    }
+}
